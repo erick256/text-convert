656b2a3e42b16ee8fcb2dd12162fb4cdb6c27042
Erick <rerick256@gmail.com>
2019-07-22 20:23:52 -0700

    Add readLine()

--- FileHandler.cpp
+++ FileHandler.cpp
@@ -75,3 +75,32 @@ std::string FileHandler::read() // reads entire file into a single string variab
     
     return data;
 }
+
+std::string FileHandler::readLine(unsigned int line) // line starts at 1
+{
+    std::string buffer;
+    std::string lineData = "";
+    
+    bool endOfFile = true;;
+    
+    int counter = 0;
+    
+    while(std::getline(iFile, buffer))
+    {
+        counter++;
+        
+        if(counter == line)
+        {
+            lineData = buffer;
+            endOfFile = false;
+            break;
+        }
+    }
+    
+    if(endOfFile)
+    {
+        std::cout << "End of file. No line found." << std::endl;
+    }
+    
+    return lineData;
+}
